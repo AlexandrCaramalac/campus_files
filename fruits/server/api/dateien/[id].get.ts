ce99d7b9345ea6a824759a16d569abda8aab4958
Darian <darian.reinsch@gmx.de>
2026-04-29 15:15:41 +0200

    SupaBase, Server profilname verbunden.

--- fruits/server/api/dateien/[id].get.ts
+++ fruits/server/api/dateien/[id].get.ts
@@ -6,7 +6,7 @@ import { serverSupabaseClient } from "#supabase/server"
 export default eventHandler(async (event) => {
   const client = await serverSupabaseClient(event);
 
-  const { data } = await client.from("dateien").select("*").eq("kursID", event.context.params.id);
+const { data } = await client.from("dateien").select("*, profile(name)").eq("kursID", event.context.params.id);
   
   // @ts-ignore
   data?.sort((a, b) => a.created_at.localeCompare(b.created_at));
